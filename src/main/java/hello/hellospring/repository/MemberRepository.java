537f02557c9fb5e643abe48fef4ee812ecad0d44
82106 <eldms0406@naver.com>
2023-09-24 18:18:46 +0900

    section 3. Member Repository interface, 구현부 구현

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    // 회원 정보 저장
+    Member save(Member member);
+
+    // 회원 정보 찾기
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
