467235bd83af81f8334b125fcc18e12631bee94f
82106 <eldms0406@naver.com>
2023-10-27 01:35:15 +0900

    section 6. 순수 JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,24 +1,32 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
     // Spring Bean에 MemberService, MemberRepository 올리고,
     // MemberService Bean에 올라온 MemberRepository를 사용하도록 함
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+    // 가장 중요! 다형성
     @Bean
-    MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
