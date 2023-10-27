7a6da8f0ff5a0da5ebbc916213b94de55124a44d
82106 <eldms0406@naver.com>
2023-10-27 18:52:05 +0900

    section 6. Spring Data JPA 이용하기

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,33 +1,27 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    // Spring Bean에 MemberService, MemberRepository 올리고,
-    // MemberService Bean에 올라온 MemberRepository를 사용하도록 함
-    @Autowired
-    EntityManager em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
     // 가장 중요! 다형성
-    @Bean
-    MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
-
+//    @Bean
+//    MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
